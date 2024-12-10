5769db851bd6826d5dd6979927f069fd2830bd8d
alianjum-web <muhammadalikhanzada7890@gmail.com>
2024-12-10 10:10:12 +0500

    change the fetch request url

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,7 @@
 const config = {
-  backendUrl: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3060',
+  // backendUrl: import.meta.env.VITE_BACKEND_URL || 'http://localhost:3060',
+  backendUrl: import.meta.env.VITE_BACKEND_URL,
+
 };
 
 console.log(`backendUrl in config.js: ${config.backendUrl}`)
